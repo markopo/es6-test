abf64dc83d5503ceb204313f2c9968caeed4c06b
Marko Poikkimäki <markopoikkimaki@Markos-MBP.lan>
2017-04-19 20:44:16 +0200

    1st commit
    Add babel, es6, web pack, jquery.

---
+++ js/main.js
@@ -0,0 +1,14 @@
+import $ from "jquery";
+
+$('p').css('color', 'red');
+
+document.getElementById('button1').onclick = () => {
+    "use strict";
+
+    alert('Hej från button');
+
+
+};
+
+
+let hello = () => { alert('hello'); };
